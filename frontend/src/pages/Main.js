21f30f225fc796e46373ba7fbd99622aeee5cde4
Joao Daniel Rufino <joaodaniel0405@gmail.com>
2019-08-10 23:48:57 -0300

    Fim :)

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
@@ -7,9 +8,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -20,6 +23,17 @@ export default function Main({ match }) {
     }
 
     loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    });
+
   }, [match.params.id]);
 
   async function handleLike(id) {
@@ -63,6 +77,16 @@ export default function Main({ match }) {
           ))}
         </ul>
       ) :  ( <div className="empty" >Acabou :(</div> )}
+
+      {matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="It's a match" />
+          <img className="avatar" src={matchDev.avatar} alt=""/>
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+          <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+        </div>
+      )}
     </div>
   );
 }
--- tindev/src/services/api.js
+++ tindev/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://10.0.3.2:3333' // Ou executar adb reverse tcp:3333 tcp:3333 e utilizar localhost
 });
 
 export default api;
